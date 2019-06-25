bc034244789080289ef60db774e54e236262f384
josephepia <remixlebo@gmail.com>
2019-06-25 11:31:49 -0500

    Add files via upload

---
+++ js/personalizado.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+
+
+	$("#sobre").on("click", function () {
+		$('#navbarSupportedContent li.active').removeClass('active');
+	 	$("#sobre").addClass("active");
+	});
+
+	$("#Marcado1").on("click", function () {
+		$('#navbarSupportedContent li.active').removeClass('active');
+	 	$("#Marcado1").addClass("active");
+
+	});
+
+	$("#Marcado2").on("click", function () {
+		$('#navbarSupportedContent li.active').removeClass('active');
+	 	$("#Marcado2").addClass("active");
+	});
+
+	$("#Marcado3").on("click", function () {
+		$('#navbarSupportedContent li.active').removeClass('active');
+	 	$("#Marcado3").addClass("active");
+	});
+
+	$("#Marcado4").on("click", function () {
+		$('#navbarSupportedContent li.active').removeClass('active');
+	 	$("#Marcado4").addClass("active");
+	});
+
+	$("#Marcado5").on("click", function () {
+		$('#navbarSupportedContent li.active').removeClass('active');
+	 	$("#Marcado5").addClass("active");
+	});
+
+
+});
